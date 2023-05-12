2f5e63a20a1239ffbcfd4b279497c0ba91f195ce
sting <bobadillasting@gmail.com>
2023-05-11 21:05:30 -0300

    Final Proyect. Nedd Dockerfile or +

--- src/main/java/com/mikellbobadilla/jwt_security/exception/MapExceptionHandler.java
+++ src/main/java/com/mikellbobadilla/jwt_security/exception/MapExceptionHandler.java
@@ -3,7 +3,6 @@ package com.mikellbobadilla.jwt_security.exception;
 import com.mikellbobadilla.jwt_security.DTO.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
